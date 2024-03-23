2aa043de4a09040fc27fe809132f2656b56b3598
xsbrx <161389313+Pplsbr@users.noreply.github.com>
2024-03-23 13:07:06 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-70cf8abd-afe5-48d4-bacc-05486d741f51/)
 
 
 echo "$curl_output" >> Update.tsx
